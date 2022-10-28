e309beab09d2fd240a26a21954bb5760330075cf
João Vinholi <jgvinholi@gmail.com>
2022-10-28 18:35:56 +0200

    Update generate_rocs.m
    .

--- scripts/generate_rocs.m
+++ scripts/generate_rocs.m
@@ -2,7 +2,8 @@
 stack_type = {'gaussian', 'rayleigh/', 'gamma/', 'weibull/', 'log_normal/'};
 len_type = length(stack_type);
 for i=1:len_type
-    roc_script(i, 'angle', strcat('C:\Users\jgvin\OneDrive\ITA\Paulo\scripts\data\stacks\passes_angle\', stack_type{i}))
+    roc_script(i, 'angle', strcat(['C:\Users\jgvin\OneDrive\ITA\Paulo\' ...
+        'sar-entropy-stakcs\scripts\data\stacks\passes_angle\'], stack_type{i}))
 end
 
 
@@ -10,5 +11,6 @@ end
 stack_type = {'gaussian/', 'rayleigh/', 'gamma/', 'weibull/', 'log_normal/'};
 len_type = length(stack_type);
 for i=1:len_type
-    roc_script(i, 'pass', strcat('C:\Users\jgvin\OneDrive\ITA\Paulo\scripts\data\stacks\passes_pass\', stack_type{i}))
+    roc_script(i, 'pass', strcat(['C:\Users\jgvin\OneDrive\ITA\Paulo\' ...
+        'sar-entropy-stacks\scripts\data\stacks\passes_pass\'], stack_type{i}))
 end
